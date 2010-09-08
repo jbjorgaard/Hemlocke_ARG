487e3281e4d04eba61573d06e2fa624fc04b3421
Jeremiah Bjorgaard <jeremiah.bjorgaard@gmail.com>
2010-09-07 21:03:24 -0500

    deleted unecessary backup of index.html created a Player class with main method that uses GetPlayerCommand to take user input from command line and print out a confirmation of user Input

---
+++ src/coreclasses/GetPlayerCommand.java
@@ -0,0 +1,26 @@
+package coreclasses;
+
+import java.io.*;
+
+public class GetPlayerCommand {
+	
+	public String enterCommand() {
+	
+		System.out.println("Enter a command: ");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String playerCommand = null;
+		
+		try {
+			playerCommand = br.readLine();
+		} catch (IOException ioe) {
+			System.out.println("There was an error trying to read your command.");
+			System.exit(1);
+		}
+		
+		System.out.println("[Your Character] " + playerCommand + ".");
+		return playerCommand;
+	}
+	
+}
---
+++ src/coreclasses/Player.java
@@ -0,0 +1,8 @@
+package coreclasses;
+
+public class Player {
+	public static void main(String[] args) {
+		GetPlayerCommand ui = new GetPlayerCommand();
+		ui.enterCommand();
+	}
+}
